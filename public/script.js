5764d7357daafc58331fca54e49bacc9a8d40c05
Prvncodz <praveenwks@gmail.com>
2025-11-16 09:56:33 +0530

    made swift glance section responsive

--- public/script.js
+++ public/script.js
@@ -12,26 +12,25 @@ let isActive=false;
 window.addEventListener("scroll",()=>{
   
   if(pageYOffset> counterContainer.offsetTop- counterContainer.offsetHeight - 200 && isActive===false){
-    console.log("if statement reached");
     counters.forEach(c=>{
       const target=c.dataset.target;
       let count=0;
       const interval= setInterval(()=>{
-        if(target<100){
+        if(target<60){
           count+=1;
         }else if(target<1100){
-          count+=2;
+          count+=15;
         }else{
-          count+=5
+          count+=50;
         }
-        count+=3;
+        
         if(count>=target){
           c.innerText=target;
           clearInterval(interval);
         }else{
           c.innerText=count;
         }
-      },0.5);
+      },20);
     });
     isActive=true;
   }
